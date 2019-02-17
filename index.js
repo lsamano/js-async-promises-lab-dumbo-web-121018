fdd599b987949bdab1363b72906da9dcecf60225
lsamano <leizl.samano@gmail.com>
2019-02-16 21:26:24 -0500

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,46 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
 
+const blank = () => {}
+
 let question;
+questionContainer = document.querySelector(".question-container")
+
+const appendQuestion = question => {
+  questionContainer.innerText = question.questionText;
+}
+
+const askQuestionThen = time => {
+  question = questions[Math.floor(Math.random()*this.length)];
+  appendQuestion(question);
+  return new Promise (() => setTimeout(blank, time))
+}
+
+const removeQuestion = () => {
+  questionContainer.innerText = ""
+  return new Promise (blank);
+}
+
+const askQuestionThenRemoveQuestion = time => {
+  return askQuestionThen(time)
+  .then(removeQuestion)
+}
+
+const trueAndFalseButtons = () => {
+  return document.querySelector(".true-false-list").querySelectorAll(".btn")
+}
+
+const toggleTrueAndFalseButtons = () => {
+  trueAndFalseButtons().forEach(button => {
+    if (button.classList.contains("hide")) {
+      button.classList.remove("hide")
+    } else {
+      button.classList.add("hide")
+    }
+  })
+}
+
+const displayQuestionOnClick = () => {
+    toggleTrueAndFalseButtons();
+    askQuestionThenRemoveQuestion(5000);
+}
